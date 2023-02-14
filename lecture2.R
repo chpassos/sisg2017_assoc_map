9ae2bbce343bea0bcd2117dcbeda2d3e85a9264a
Carlos Passos <crlshnrqpassos@gmail.com>
2023-02-14 14:27:29 -0300

    Create lecture2.R

---
+++ lecture2.R
@@ -0,0 +1,30 @@
+# Lecture 2.  Hispanic Community Health Study: HCHS/SOL dataset
+
+## Ok, so this is an lecture about HCHS/SOL dataset
+## I can't get much only reading the slides, although having a lot of 
+## information in them.
+## I don't know what is the point they are trying to make, 
+## neither what is the takehome message.
+
+# Some general information about the HCHS/SOL dataset:
+## 1. Growing community of Latinos in US
+## 2. N=16415 participaints
+## 3. Sampling based in four US cities
+## 4. It is a longitudinal study (follow up interviews are done)
+## 5. There's a list of phenotypes. Below, two that appears to caught attention 
+#### 5.a Prevalent and incident pulmonary diagnoses + exacerbations
+#### 5.b Pulmonary Function Testing (basal and post-bronchodilator)
+## 6. Participants self-identified with 6 different "ancestries"
+#### 6.a Central American
+#### 6.b Cuban
+#### 6.c Dominican
+#### 6.d Mexican
+#### 6.e Puerto Rican
+#### 6.f South American
+#### 6.g Other/Multiple
+## 7. They give importance about asthma...
+#### 7.a So they throw us a bunch of info about asthma
+#### 7.b Apparently, there's difference in asthma prevalence between Mexican and Puerto Ricans
+## 8. They also show three approaches used in HCHS/SOL, but not relevant enough for me to type it (maybe?)
+
+# And I think that's the lecture about the HCHS/SOL dataset!
